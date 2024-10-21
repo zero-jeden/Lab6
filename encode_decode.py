5444625ec8cb9663e417d0d8e821146b48f4e474
zero-jeden <101535397+zero-jeden@users.noreply.github.com>
2024-10-21 15:20:54 -0400

    Add files via upload

---
+++ encode_decode.py
@@ -0,0 +1,49 @@
+def encode(password):
+    storage_array =[]
+    string =''
+    for i in range(0, len(password)):
+        storage_array.append(3 + int(password[i]))
+
+    for k in range(0, len(storage_array)):
+        if storage_array[k] >= 10:
+            string += str(storage_array[k] - 10)
+        else:
+            string += str(storage_array[k])
+
+
+    return string
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        print('Menu')
+        print('------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+
+
+
+        selection = int(input('Please enter an option: '))
+        if selection == 1:
+            password = input('Please enter your password to encode: ')
+            encrypted = encode(password)
+            print('Your password has been encoded and stored!')
+
+        if selection == 2:
+            print(f'The encoded  password is {'partner function'}, and the original password is {encrypted}.')
+
+        if selection == 3:
+            break
+
+
+
+
+'''
+Test cases:
+password = '00009962'
+password1 = '12345555'
+print(encode(password))
+print(encode(password1))'''
